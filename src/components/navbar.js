a2a54912bd8a35d58d319f432551938cb1fd7831
Jia Yuan <jia.yuan@stablecorp.ca>
2023-06-24 12:39:27 -0400

    add navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { AppBar, Box, Button, Container, Toolbar, Typography } from '@mui/material';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = ['Borrow', 'Lend', 'Pools'];
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            SPARKLER
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar
