5e50dc03933b704870a6cc7531db440383146c4b
wenting <e06564447@u.nus.edu>
2023-06-25 02:45:24 -0400

    added events

--- src/tokens.js
+++ src/tokens.js
@@ -1,53 +1,38 @@
 import React, { useEffect } from "react"
 import { init } from "@airstack/airstack-react"
 import { useQuery } from "@airstack/airstack-react"
-import { Box, Container, Grid,Card, CardContent, Typography } from "@mui/material"
+import { Box, Container, Grid, Card, CardContent, Typography } from "@mui/material"
+import moment from "moment"
 
 init("598406f101364281b160f60f0761fe96")
 
 export default function Tokens({ address }) {
   const query = `
-  query WalletQuery {
-    Wallet(input: { identity: "${address}", blockchain: ethereum }) {
-      socials {
-        dappName
-        profileName
-        profileCreatedAtBlockTimestamp
-        userAssociatedAddresses
-      }
-      tokenBalances {
-        tokenAddress
-        amount
-        tokenId
-        tokenType
-        tokenNfts {
-          contentValue {
-            image {
-              original
+    query WalletQuery {
+      Wallet(input: { identity: "${address}", blockchain: ethereum }) {
+        socials {
+          dappName
+          profileName
+          profileCreatedAtBlockTimestamp
+          userAssociatedAddresses
+        }
+        tokenBalances {
+          tokenAddress
+          amount
+          tokenId
+          tokenType
+          tokenNfts {
+            contentValue {
+              image {
+                original
+              }
+            }
+            token {
+              name
             }
           }
-          token {
-            name
-          }
         }
-      }
-      poaps {
-        id
-        chainId
-        blockchain
-        dappName
-        dappSlug
-        dappVersion
-        eventId
-        owner {
-          identity
-        }
-        createdAtBlockTimestamp
-        createdAtBlockNumber
-        tokenId
-        tokenAddress
-        tokenUri
-        poapEvent {
+        poaps {
           id
           chainId
           blockchain
@@ -55,25 +40,41 @@ export default function Tokens({ address }) {
           dappSlug
           dappVersion
           eventId
-          metadata
-          contentType
-          contentValue {
-            image {
-              original
-            }
+          owner {
+            identity
           }
-          eventName
-          description
-          country
-          city
-          startDate
-          endDate
-          isVirtualEvent
-          eventURL
+          createdAtBlockTimestamp
+          createdAtBlockNumber
+          tokenId
+          tokenAddress
+          tokenUri
+          poapEvent {
+            id
+            chainId
+            blockchain
+            dappName
+            dappSlug
+            dappVersion
+            eventId
+            metadata
+            contentType
+            contentValue {
+              image {
+                original
+              }
+            }
+            eventName
+            description
+            country
+            city
+            startDate
+            endDate
+            isVirtualEvent
+            eventURL
+          }
         }
       }
     }
-  }
   `
 
   const { data, loading, error } = useQuery(query)
@@ -86,7 +87,6 @@ export default function Tokens({ address }) {
         .map((token) => token.tokenNfts.contentValue.image.original)
 
       Promise.all(tokenNfts.map(fetchImage)).then((images) => {
-        console.log("Fetched images:", images)
       })
     }
   }, [data])
@@ -121,17 +121,14 @@ export default function Tokens({ address }) {
           <Grid container spacing={2} sx={{ py: 2 }}>
             {data.Wallet.tokenBalances.map((token, index) => {
               if (token.tokenType === "ERC20") {
-                return null; // Skip rendering ERC20 tokens
+                return null // DON'T WANT ERC20 tokens
               }
               return (
                 <Grid item xs={12} sm={6} md={4} key={index}>
                   <Card sx={{ height: "400px" }}>
                     <CardContent>
-                      <Typography variant="h6" component="h2">
-                        Token
-                      </Typography>
                       <Typography variant="body1" component="p">
-                        Token Address:{" "}
+                        Token Address:
                         <span style={{ wordBreak: "break-word" }}>{token.tokenAddress}</span>
                       </Typography>
                       <Typography variant="body1" component="p">
@@ -157,11 +154,55 @@ export default function Tokens({ address }) {
                     </CardContent>
                   </Card>
                 </Grid>
-              );
+              )
             })}
+          </Grid>
+          <Typography variant="h5">Events</Typography>
+          <Grid container spacing={2} sx={{ py: 2 }}>
+            {data.Wallet.poaps.map((poap, index) => (
+              <Grid item xs={12} sm={6} md={4} key={index}>
+                <Card sx={{ height: "550px" }}>
+                  <CardContent>
+                    <Typography variant="h6" component="h2">
+                      Event {index + 1}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                      Event Name: {poap.poapEvent.eventName}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                      Event ID: {poap.poapEvent.eventId}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                      Start Date - End Date: {moment(poap.poapEvent.startDate).format("DD/MMM/YYYY")} -
+                      {moment(poap.poapEvent.endDate).format("DD/MMM/YYYY")}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                      Country, City: {poap.poapEvent.country}, {poap.poapEvent.city}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                      Description: {poap.poapEvent.description}
+                    </Typography>
+                    {poap.poapEvent.contentValue && poap.poapEvent.contentValue.image && (
+                      <div>
+                        <Typography variant="body1" component="p">
+                          Event Image:
+                        </Typography>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                          <img
+                            src={poap.poapEvent.contentValue.image.original}
+                            alt={`Event ${index + 1}`}
+                            style={{ maxWidth: "100%", maxHeight: "150px" }}
+                          />
+                        </div>
+                      </div>
+                    )}
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Box>
       )}
-    </Container>  
+    </Container>
   )
 }
